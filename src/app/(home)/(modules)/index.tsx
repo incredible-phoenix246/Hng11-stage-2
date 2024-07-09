3ca0dd0b27183a83e887ab9d91af281aef013e34
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-07-09 10:16:04 +0100

    final changes

--- src/app/(home)/(modules)/index.tsx
+++ src/app/(home)/(modules)/index.tsx
@@ -658,6 +658,27 @@ const ProductDetails = () => {
 const CartPage = () => {
   const [cart, setCart] = useState<CartItem[]>([]);
   const searchParams = useSearchParams().get("path");
+
+  const [formState, setFormState] = useState<PaymentDetailsState>({
+    email: "",
+    cardHolder: "",
+    cardNumber: "",
+    cardExpiry: "",
+    cardCvc: "",
+    billingAddress: "",
+    billingState: "",
+    billingZip: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
   useEffect(() => {
     const getCart = () => {
       const cart = getObjectFromLocalStorage<CartItem[]>("cart");
@@ -703,6 +724,16 @@ const CartPage = () => {
   };
 
   const { totalPrice, totalQuantity } = calculateSubtotal();
+  const {
+    email,
+    cardHolder,
+    cardNumber,
+    cardExpiry,
+    cardCvc,
+    billingAddress,
+    billingState,
+    billingZip,
+  } = formState;
 
   return (
     <section className="container flex flex-col">
@@ -812,19 +843,25 @@ const CartPage = () => {
           ) : (
             <div className="flex flex-col items-center w-full h-full">
               your cart is empty start shoping
+              <Button asChild variant="ghost" className="w-[150px] shadow-md">
+                <Link href="/?path=home">
+                  <ArrowLeft2 />
+                  Start shoping
+                </Link>
+              </Button>
             </div>
           )}
         </div>
 
-        <div className="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0">
-          <p className="text-xl font-medium">Payment Details</p>
-          <p className="text-gray-400">
+        <div className="mt-10 bg-primary px-4 pt-8 lg:mt-0">
+          <p className="text-xl font-medium text-secondary">Payment Details</p>
+          <p className="text-base-white">
             Complete your order by providing your payment details.
           </p>
           <div className="">
             <label
               htmlFor="email"
-              className="mt-4 mb-2 block text-sm font-medium"
+              className="mt-4 mb-2 block text-sm font-medium text-base-white"
             >
               Email
             </label>
@@ -833,21 +870,23 @@ const CartPage = () => {
                 type="text"
                 id="email"
                 name="email"
+                value={email}
+                onChange={handleChange}
                 className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
                 placeholder="your.email@gmail.com"
               />
               <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 text-gray-400"
+                  className="h-4 w-4 text-base-white"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  stroke-width="2"
+                  strokeWidth="2"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
                   />
                 </svg>
@@ -855,7 +894,7 @@ const CartPage = () => {
             </div>
             <label
               htmlFor="card-holder"
-              className="mt-4 mb-2 block text-sm font-medium"
+              className="mt-4 mb-2 block text-sm font-medium text-base-white"
             >
               Card Holder
             </label>
@@ -864,21 +903,23 @@ const CartPage = () => {
                 type="text"
                 id="card-holder"
                 name="card-holder"
+                onChange={handleChange}
+                value={cardHolder}
                 className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
                 placeholder="Your full name here"
               />
               <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 text-gray-400"
+                  className="h-4 w-4 text-base-white"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  stroke-width="2"
+                  strokeWidth="2"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z"
                   />
                 </svg>
@@ -886,7 +927,7 @@ const CartPage = () => {
             </div>
             <label
               htmlFor="card-no"
-              className="mt-4 mb-2 block text-sm font-medium"
+              className="mt-4 mb-2 block text-sm font-medium text-base-white"
             >
               Card Details
             </label>
@@ -896,12 +937,14 @@ const CartPage = () => {
                   type="text"
                   id="card-no"
                   name="card-no"
+                  value={cardNumber}
+                  onChange={handleChange}
                   className="w-full rounded-md border border-gray-200 px-2 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
                   placeholder="xxxx-xxxx-xxxx-xxxx"
                 />
                 <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                   <svg
-                    className="h-4 w-4 text-gray-400"
+                    className="h-4 w-4 text-base-white"
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
@@ -916,19 +959,23 @@ const CartPage = () => {
               <input
                 type="text"
                 name="credit-expiry"
+                value={cardExpiry}
+                onChange={handleChange}
                 className="w-full rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
                 placeholder="MM/YY"
               />
               <input
                 type="text"
                 name="credit-cvc"
+                value={cardCvc}
+                onChange={handleChange}
                 className="w-1/6 flex-shrink-0 rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
                 placeholder="CVC"
               />
             </div>
             <label
               htmlFor="billing-address"
-              className="mt-4 mb-2 block text-sm font-medium"
+              className="mt-4 mb-2 block text-sm font-medium text-base-white"
             >
               Billing Address
             </label>
@@ -938,6 +985,8 @@ const CartPage = () => {
                   type="text"
                   id="billing-address"
                   name="billing-address"
+                  value={billingAddress}
+                  onChange={handleChange}
                   className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
                   placeholder="Street Address"
                 />
@@ -951,6 +1000,8 @@ const CartPage = () => {
               </div>
               <select
                 name="billing-state"
+                value={billingState}
+                onChange={handleChange}
                 className="w-full rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
               >
                 <option value="State">State</option>
@@ -958,6 +1009,8 @@ const CartPage = () => {
               <input
                 type="text"
                 name="billing-zip"
+                value={billingZip}
+                onChange={handleChange}
                 className="flex-shrink-0 rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none sm:w-1/6 focus:z-10 focus:border-blue-500 focus:ring-blue-500"
                 placeholder="ZIP"
               />
@@ -965,20 +1018,24 @@ const CartPage = () => {
 
             <div className="mt-6 border-t border-b py-2">
               <div className="flex items-center justify-between">
-                <p className="text-sm font-medium text-gray-900">Subtotal</p>
-                <p className="font-semibold text-gray-900">$399.00</p>
+                <p className="text-sm font-medium text-base-white">Subtotal</p>
+                <p className="font-semibold text-base-white">
+                  &#x20A6;{totalPrice}
+                </p>
               </div>
               <div className="flex items-center justify-between">
-                <p className="text-sm font-medium text-gray-900">Shipping</p>
-                <p className="font-semibold text-gray-900">$8.00</p>
+                <p className="text-sm font-medium text-base-white">Shipping</p>
+                <p className="font-semibold text-base-white">&#x20A6;1000.00</p>
               </div>
             </div>
             <div className="mt-6 flex items-center justify-between">
-              <p className="text-sm font-medium text-gray-900">Total</p>
-              <p className="text-2xl font-semibold text-gray-900">$408.00</p>
+              <p className="text-sm font-medium text-base-white">Total</p>
+              <p className="text-2xl font-semibold text-base-white">
+                &#x20A6;{totalPrice + 1000}
+              </p>
             </div>
           </div>
-          <button className="mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white">
+          <button className="mt-4 mb-8 w-full rounded-md bg-secondary px-6 py-3 font-medium text-white">
             Place Order
           </button>
         </div>
@@ -996,3 +1053,14 @@ export {
   ProductDetails,
   CartPage,
 };
+
+interface PaymentDetailsState {
+  email: string;
+  cardHolder: string;
+  cardNumber: string;
+  cardExpiry: string;
+  cardCvc: string;
+  billingAddress: string;
+  billingState: string;
+  billingZip: string;
+}
